f9eaaecdedf73b1191e0f75fca71ff2c46b7da97
gass-0000 <gassuuuuu@gmail.com>
2026-07-13 12:59:29 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yotei-quest-v1.1.1';
+const CACHE='yotei-quest-v1.1.3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./town-map.png','./restaurant-map.png','./tree-wind.png','./river-light.png','./library-map.png','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
